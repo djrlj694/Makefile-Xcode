4af763cdff94d7e1d65b678d52777bbb90fe978f
djrlj694 <robert.l.jones@synthelytics.com>
2019-02-23 15:59:50 -0500

    Create CocoaPods.mk

---
+++ CocoaPods.mk
@@ -0,0 +1,64 @@
+# CocoaPods.mk
+# Copyright © 2019 Synthelytics LLC. All rights reserved.
+#
+# ==============================================================================
+# PROGRAM: CocoaPods
+# AUTHORS: Robert L. Jones
+# COMPANY: Synthelytics LLC
+# VERSION: 1.1.0
+# CREATED: 04FEB2019
+# REVISED: 23FEB2019
+# ==============================================================================
+
+# ==============================================================================
+# Variables
+# ==============================================================================
+
+# Files
+
+COCOAPODS_FILES = Framework.podspec
+
+# ==============================================================================
+# Phony Targets
+#
+# A phony target is a convenient name for a set of commands to be executed when
+# an explicit request is made.  Its commands won't run if a file of the same
+# name exists.  Two reasons to use a phony target are:
+#
+# 1. To avoid a conflict with a file of the same name;
+# 2. To improve performance.
+# ==============================================================================
+
+# Prerequisite phony targets for cleaning activities
+
+.PHONY: clean-cocoapods
+
+clean-cocoapods: | $(LOG) ## Completes all CocoaPods cleanup activities.
+	@printf "Removing CocoaPods setup..."
+	@rm -rf $(COCOAPODS_FILES) >$(LOG) 2>&1; \
+	$(RESULT)
+
+# Prerequisite phony targets for setup activities
+
+.PHONY: init-cocoapods 
+
+init-cocoapods: $(COCOAPODS_FILES) ## Completes all initial CocoaPods setup activities.
+
+# ==============================================================================
+# File Targets
+# ==============================================================================
+
+Framework.podspec: Framework.podspec.download ## Makes a Framework.podspec file.
+
+# ==============================================================================
+# Intermediate Targets
+#
+# An intermediate target corresponds to a file that is needed on the way from a
+# source file to a target file.  It typically is a temporary file that is needed
+# only once to generate the target after the source changed.  The "make" command
+# automatically removes files that are identified as intermediate targets.  In
+# other words, such files that did not exist before a "make" run executed do not
+# exist after a "make" run.
+# ==============================================================================
+
+.INTERMEDIATE: Framework.podspec.download
