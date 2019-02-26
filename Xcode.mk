dc2d81e88ba576d17b605bb41dc817fdf072ce50
djrlj694 <robert.l.jones@synthelytics.com>
2019-02-26 07:26:55 -0500

    Update Xcode.mk

--- Xcode.mk
+++ Xcode.mk
@@ -7,7 +7,7 @@
 # COMPANY: Synthelytics LLC
 # VERSION: 1.1.0
 # CREATED: 04FEB2019
-# REVISED: 23FEB2019
+# REVISED: 26FEB2019
 # ==============================================================================
 
 # ==============================================================================
@@ -21,3 +21,27 @@ RESOURCES_DIRS = $(addprefix $(PROJECT)/Resources/,$(addsuffix /.,$(RESOURCES)))
 
 SOURCES = Controllers Extensions Models Protocols ViewModels Views
 SOURCES_DIRS = $(addprefix $(PROJECT)/Sources/,$(addsuffix /.,$(SOURCES)))
+
+# ==============================================================================
+# Phony Targets
+#
+# A phony target is a convenient name for a set of commands to be executed when
+# an explicit request is made.  Its commands won't run if a file of the same
+# name exists.  Two reasons to use a phony target are:
+#
+# 1. To avoid a conflict with a file of the same name;
+# 2. To improve performance.
+# ==============================================================================
+
+# Prerequisite phony targets for initial setup activities
+
+.PHONY: init-xcode init-xcode-vars
+
+init-xcode: init-xcode-vars init-xcode-dirs init-carthage init-cocoapods ## Completes all initial Xcode setup activites.
+	@echo "FILE_URL =  $(FILE_URL)"
+
+init-xcode-dirs: $(RESOURCES_DIRS) $(SOURCES_DIRS)
+
+init-xcode-vars: ## Completes all Xcode variable setup activites.
+	$(eval TEMPLATES_REPO = $(GITHUB_USER)/Cookiecutter-Xcode)
+	$(eval FILE_URL = https://raw.githubusercontent.com/$(TEMPLATES_REPO)/master/templates)
