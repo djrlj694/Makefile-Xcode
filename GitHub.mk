a8457fac77ca2047ad9c29b8b4e939456ae22300
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-03 07:55:23 -0500

    Update GitHub.mk

--- GitHub.mk
+++ GitHub.mk
@@ -14,14 +14,18 @@
 # Variables
 # ==============================================================================
 
+# ------------------------------------------------------------------------------
 # Accounts
+# ------------------------------------------------------------------------------
 
 GITHUB_USER = $(USER)
 TRAVIS_USER = $(USER)
 
 EMAIL = $(USER)@gmail.com
 
+# ------------------------------------------------------------------------------
 # Directories
+# ------------------------------------------------------------------------------
 
 GITHUB_DIR1 = .github
 GITHUB_DIR2 = $(GITHUB_DIR1)/ISSUE_TEMPLATE
@@ -29,7 +33,9 @@ GITHUB_DIR3 = $(GITHUB_DIR1)/PULL_REQUEST_TEMPLATE
 
 GITHUB_DIRS = $(addsuffix /.,$(GITHUB_DIR2) $(GITHUB_DIR3))
 
+# ------------------------------------------------------------------------------
 # Files
+# ------------------------------------------------------------------------------
 
 #DOCS0 = CHANGELOG README REFERENCES SUPPORT
 DOCS0 = CHANGELOG CODE_OF_CONDUCT CONTRIBUTING README REFERENCES SUPPORT
@@ -42,6 +48,20 @@ DOCS3 = $(addprefix $(GITHUB_DIR3)/,pull_request_template)
 # https://raw.githubusercontent.com/djrlj694/Cookiecutter-GitHub/master/%7B%7Bcookiecutter.project_name%7D%7D/.github/PULL_REQUEST_TEMPLATE/pull_request_template.md
 
 GITHUB_FILES = $(addsuffix .md,$(DOCS0) $(DOCS1) $(DOCS2) $(DOCS3))
+GITHUB_DOWNLOADED_FILES = $(addsuffix .download,$(GITHUB_FILES))
+
+# ==============================================================================
+# Macros
+# ==============================================================================
+
+define update-github-file
+	@sed -e 's/{{ cookiecutter.project_name }}/$(PROJECT)/g' $@ >$@.tmp1
+	@sed -e 's/{{ cookiecutter.github_user }}/$(GITHUB_USER)/g' $@.tmp1 >$@.tmp2
+	@sed -e 's/{{ cookiecutter.email }}/$(EMAIL)/g' $@.tmp2 >$@.tmp3
+	@sed -e 's/{{ cookiecutter.github_user }}/$(GITHUB_USER)/g' $@.tmp3 >$@.tmp4
+	@sed -e 's/{{ cookiecutter.travis_user }}/$(TRAVIS_USER)/g' $@.tmp4 >$@
+	@rm -rf $@.tmp*
+endef
 
 # ==============================================================================
 # Phony Targets
@@ -54,7 +74,9 @@ GITHUB_FILES = $(addsuffix .md,$(DOCS0) $(DOCS1) $(DOCS2) $(DOCS3))
 # 2. To improve performance.
 # ==============================================================================
 
+# ------------------------------------------------------------------------------
 # Prerequisite phony targets for cleaning activities
+# ------------------------------------------------------------------------------
 
 .PHONY: clean-docs-github
 
@@ -63,7 +85,9 @@ clean-docs-github: | $(LOG) ## Completes all GitHub Markdown cleanup activities.
 	@rm -rf $(GITHUB_FILES) $(GITHUB_DIR1) >$(LOG) 2>&1; \
 	$(RESULT)
 
+# ------------------------------------------------------------------------------
 # Prerequisite phony targets for initial setup activities
+# ------------------------------------------------------------------------------
 
 .PHONY: init-github-dirs init-github-vars
 
@@ -77,68 +101,38 @@ init-github-vars: ## Completes all GitHub variable setup activites.
 	$(eval FILE_URL = https://raw.githubusercontent.com/$(TEMPLATES_REPO)/master/%7B%7Bcookiecutter.project_name%7D%7D)
 	$(eval ORIGIN_URL = https://github.com/$(PROJECT_REPO).git)
 
+# ------------------------------------------------------------------------------
 # Prerequisite phony targets for document generation activities
+# ------------------------------------------------------------------------------
 
 .PHONY: docs-github 
 
-docs-github: $(GITHUB_FILES) | $$(@D)/. ## Completes all GitHub document generation activites.
+docs-github: $(GITHUB_FILES) ## Completes all GitHub document generation activites.
 
 # ==============================================================================
 # File Targets
 # ==============================================================================
 
-#$(GITHUB_DIR1)/CODE_OF_CONDUCT.md: CODE_OF_CONDUCT.md.download | $$(@D)/. ## Makes a CODE_OF_CONDUCT.md file.
-CODE_OF_CONDUCT.md: CODE_OF_CONDUCT.md.download | $$(@D)/. ## Makes a CODE_OF_CONDUCT.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	@mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
-	@sed -e 's/{{ cookiecutter.project_name }}/$(PROJECT)/g' $@ >$@.tmp1
-	@sed -e 's/{{ cookiecutter.github_user }}/$(GITHUB_USER)/g' $@.tmp1 >$@.tmp2
-	@sed -e 's/{{ cookiecutter.email }}/$(EMAIL)/g' $@.tmp2 >$@
-	@rm -rf $@.tmp*
+CODE_OF_CONDUCT.md: CODE_OF_CONDUCT.md.download ## Makes a CODE_OF_CONDUCT.md file.
+	$(update-github-file)
 
-#$(GITHUB_DIR1)/CONTRIBUTING.md: CONTRIBUTING.md.download | $$(@D)/. ## Makes a CONTRIBUTING.md file.
-CONTRIBUTING.md: CONTRIBUTING.md.download | $$(@D)/. ## Makes a CONTRIBUTING.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	@mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
-	@sed -e 's/{{ cookiecutter.project }}/$(PROJECT)/g' $@ >$@.tmp1
-	@sed -e 's/{{ cookiecutter.github_user }}/$(GITHUB_USER)/g' $@.tmp1 >$@.tmp2
-	@sed -e 's/{{ cookiecutter.email }}/$(EMAIL)/g' $@.tmp2 >$@
-	@rm -rf $@.tmp*
+CONTRIBUTING.md: CONTRIBUTING.md.download ## Makes a CONTRIBUTING.md file.
+	$(update-github-file)
 
-$(GITHUB_DIR2)/bug_report.md: $(GITHUB_DIR2)/bug_report.md.download | $$(@D)/. ## Makes a bug_report.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
+$(GITHUB_DIR2)/bug_report.md: $(GITHUB_DIR2)/bug_report.md.download ## Makes a bug_report.md file.
 
-$(GITHUB_DIR2)/custom.md: $(GITHUB_DIR2)/custom.md.download | $$(@D)/. ## Makes a custom.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
+$(GITHUB_DIR2)/custom.md: $(GITHUB_DIR2)/custom.md.download ## Makes a custom.md file.
 
-$(GITHUB_DIR2)/feature_request.md: $(GITHUB_DIR2)/feature_request.md.download | $$(@D)/. ## Makes a feature_request.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
+$(GITHUB_DIR2)/feature_request.md: $(GITHUB_DIR2)/feature_request.md.download ## Makes a feature_request.md file.
 
 $(GITHUB_DIR2)/ISSUE_TEMPLATE.md: $(GITHUB_DIR2)/ISSUE_TEMPLATE.md.download ## Makes a ISSUE_TEMPLATE.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
 
-$(GITHUB_DIR3)/pull_request_template.md: $(GITHUB_DIR3)/pull_request_template.md.download | $$(@D)/. ## Makes a pull_request_template.md file.
-#	@printf "Moving file $(FILE_VAR) to directory $(DIR_VAR)..."
-#	mv $(shell basename $(@F)) $(@D); \
-#	$(RESULT)
+$(GITHUB_DIR3)/pull_request_template.md: $(GITHUB_DIR3)/pull_request_template.md.download ## Makes a pull_request_template.md file.
 
 CHANGELOG.md: CHANGELOG.md.download ## Makes a CHANGELOG.md file.
 
 README.md: README.md.download ## Makes a README.md file.
-	@sed -e 's/{{ cookiecutter.project }}/$(PROJECT)/g' $@ >$@.tmp1
-	@sed -e 's/{{ cookiecutter.github_user }}/$(GITHUB_USER)/g' $@.tmp1 >$@.tmp2
-	@sed -e 's/{{ cookiecutter.travis_user }}/$(TRAVIS_USER)/g' $@.tmp2 >$@
-	@rm -rf $@.tmp*
+	$(update-github-file)
 
 REFERENCES.md: REFERENCES.md.download ## Makes a REFERRENCES.md file.
 
@@ -155,24 +149,4 @@ SUPPORT.md: SUPPORT.md.download ## Makes a SUPPORT.md file.
 # exist after a "make" run.
 # ==============================================================================
 
-.INTERMEDIATE: bug_report.md.download
-
-.INTERMEDIATE: CHANGELOG.md.download
-
-.INTERMEDIATE: CODE_OF_CONDUCT.md.download
-
-.INTERMEDIATE: CONTRIBUTING.md.download
-
-.INTERMEDIATE: custom.md.download
-
-.INTERMEDIATE: feature_request.md.download
-
-.INTERMEDIATE: ISSUE_TEMPLATE.md.download
-
-.INTERMEDIATE: pull_request_template.md.download
-
-.INTERMEDIATE: README.md.download
-
-.INTERMEDIATE: REFERENCES.md.download
-
-.INTERMEDIATE: SUPPORT.md.download
+.INTERMEDIATE: $(GITHUB_DOWNLOADED_FILES)
