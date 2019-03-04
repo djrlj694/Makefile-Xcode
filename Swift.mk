97bb79d0d2ac5803bff5ffa51361f9aa0d708259
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-03 19:40:38 -0500

    Create Swift.mk

---
+++ Swift.mk
@@ -0,0 +1,40 @@
+# Swift.mk
+# Copyright © 2019 Synthelytics LLC. All rights reserved.
+#
+# ==============================================================================
+# PROGRAM: Swift.mk
+# AUTHORS: Robert L. Jones
+# COMPANY: Synthelytics LLC
+# VERSION: 1.0.0
+# CREATED: 03MAR2019
+# REVISED: 03MAR2019
+# ==============================================================================
+
+# ==============================================================================
+# Phony Targets
+#
+# A phony target is a convenient name for a set of commands to be executed when
+# an explicit request is made.  Its commands won't run if a file of the same
+# name exists.  Two reasons to use a phony target are:
+#
+# 1. To avoid a conflict with a file of the same name;
+# 2. To improve performance.
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Prerequisite phony targets for documentation generation activities
+# ------------------------------------------------------------------------------
+
+.PHONY: docs-swift
+
+docs-swift: | $(LOG) ## Makes Swift API documentation.
+	@printf "Generating API documentation..."
+	@jazzy \
+		--min-acl internal \
+		--no-hide-documentation-coverage \
+		--theme fullwidth \
+		--output ./docs \
+        --documentation=./*.md \
+		>$(LOG) 2>&1; \
+	$(RESULT)
+	@rm -rf ./build
