9f6892936177c84aaffa8ac53872ab36aa636c9b
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-06 12:41:24 -0500

    Update Xcode.mk

--- Xcode.mk
+++ Xcode.mk
@@ -25,6 +25,53 @@ XCODE_SOURCES = Controllers Extensions Models Protocols ViewModels Views
 XCODE_SOURCES_DIRS = $(addprefix $(PACKAGE)/Sources/,$(XCODE_SOURCES))
 
 XCODE_DIRS = $(addsuffix /.,$(XCODE_RESOURCES_DIRS) $(XCODE_SOURCES_DIRS))
+#XCODE_DIRS = $(XCODE_RESOURCES_DIRS) $(XCODE_SOURCES_DIRS)
+
+# ------------------------------------------------------------------------------
+# Test strings
+# ------------------------------------------------------------------------------
+
+define XCODE_DIRS_TEST
+.
+./.git
+./.github
+./.github/ISSUE_TEMPLATE
+./.github/PULL_REQUEST_TEMPLATE
+./$(PACKAGE)
+./$(PACKAGE)/Resources
+./$(PACKAGE)/Resources/Data
+./$(PACKAGE)/Resources/Fonts
+./$(PACKAGE)/Resources/Localization
+./$(PACKAGE)/Resources/Media
+./$(PACKAGE)/Resources/UserInterfaces
+./$(PACKAGE)/Sources
+./$(PACKAGE)/Sources/Controllers
+./$(PACKAGE)/Sources/Extensions
+./$(PACKAGE)/Sources/Models
+./$(PACKAGE)/Sources/Protocols
+./$(PACKAGE)/Sources/ViewModels
+./$(PACKAGE)/Sources/Views
+endef
+export XCODE_DIRS_TEST
+
+define XCODE_FILES_TEST
+./.github/ISSUE_TEMPLATE/ISSUE_TEMPLATE.md
+./.github/ISSUE_TEMPLATE/bug_report.md
+./.github/ISSUE_TEMPLATE/custom.md
+./.github/ISSUE_TEMPLATE/feature_request.md
+./.github/PULL_REQUEST_TEMPLATE/pull_request_template.md
+./.gitignore
+./CHANGELOG.md
+./CODE_OF_CONDUCT.md
+./CONTRIBUTING.md
+./Cartfile
+./Cartfile.private
+./Framework.podspec
+./README.md
+./REFERENCES.md
+./SUPPORT.md
+endef
+export XCODE_FILES_TEST
 
 # ==============================================================================
 # Macros
@@ -76,6 +123,53 @@ init-xcode-dirs: $(XCODE_DIRS)
 init-xcode-vars: ## Completes all Xcode variable setup activites.
 	$(eval TEMPLATES_REPO = $(GITHUB_USER)/Cookiecutter-Xcode)
 	$(eval FILE_URL = https://raw.githubusercontent.com/$(TEMPLATES_REPO)/master/%7B%7Bcookiecutter.project_name%7D%7D)
+
+# ------------------------------------------------------------------------------
+# Prerequisite phony targets for the "test" target
+# ------------------------------------------------------------------------------
+
+.PHONY: test-xcode test-xcode-dirs test-xcode-files
+
+test-xcode: test-xcode-dirs test-xcode-files ## Completes all Xcode test activites.
+
+test-xcode-dirs: expected_xcode_dirs.txt actual_xcode_dirs.txt ## Test Xcode directory setup.
+	@printf "Testing Xcode directory setup..."
+	@diff expected_xcode_dirs.txt actual_xcode_dirs.txt >$(LOG) 2>&1; \
+	$(RESULT)
+
+test-xcode-files: expected_xcode_files.txt actual_xcode_files.txt ## Test Xcode file setup.
+	@printf "Testing Xcode file setup..."
+	@diff expected_xcode_files.txt actual_xcode_files.txt >$(LOG) 2>&1; \
+	$(RESULT)
+
+# ==============================================================================
+# Intermediate Targets
+#
+# An intermediate target corresponds to a file that is needed on the way from a
+# source file to a target file.  It typically is a temporary file that is needed
+# only once to generate the target after the source changed.  The "make" command
+# automatically removes files that are identified as intermediate targets.  In
+# other words, such files that did not exist before a "make" run executed do not
+# exist after a "make" run.
+# ==============================================================================
+
+.INTERMEDIATE: actual_xcode_dirs.txt actual_xcode_files.txt actual_xcode_dirs.txt expected_xcode_dirs.txt
+
+actual_xcode_dirs.txt: ## Makes a temporary file listing expected.
+	@printf "Making file $(TARGET_VAR).\n"
+	@find . -type d -not -path "./.git/*" | sort >$@
+
+actual_xcode_files.txt: ## Makes a temporary file listing expected.
+	@printf "Making file $(TARGET_VAR).\n"
+	@find . -type f -not \( -path "./.git/*" -or -path "./make.log" -or -path "./*_dirs.txt" -or -path "./*_files.txt" \) | sort >$@
+
+expected_xcode_dirs.txt: ## Makes a temporary file listing expected.
+	@printf "Making file $(TARGET_VAR).\n"
+	@echo "$$XCODE_DIRS_TEST" >$@
+
+expected_xcode_files.txt: ## Makes a temporary file listing expected.
+	@printf "Making file $(TARGET_VAR).\n"
+	@echo "$$XCODE_FILES_TEST" >$@
 
 # ==============================================================================
 # Makefiles
