fac1025777c111568079f1a47cc0c8a4c613bcf3
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-12 18:13:40 -0400

    Update Swift.mk

--- .make/Swift.mk
+++ .make/Swift.mk
@@ -7,15 +7,15 @@
 # COMPANY: Synthelytics LLC
 # VERSION: 1.0.0
 # CREATED: 03MAR2019
-# REVISED: 11MAR2019
+# REVISED: 12MAR2019
 # ==============================================================================
 
 # ==============================================================================
 # Phony Targets
 #
 # A phony target is one that does not represent a file or directory. It can be
-# thought of as an embedded shell script to be run when an explicit request
-# is made unless uness a file of the same name exists.
+# thought of as an embedded shell script. It runs when an explicit request is
+# made unless a file of the same name exists.
 #
 # Two reasons to use a phony target are:
 #
