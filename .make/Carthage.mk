c51245da390594a4706eb7becbadff82e472a3ad
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-12 18:12:45 -0400

    Update Carthage.mk

--- .make/Carthage.mk
+++ .make/Carthage.mk
@@ -7,7 +7,7 @@
 # COMPANY: Synthelytics LLC
 # VERSION: 1.1.0
 # CREATED: 04FEB2019
-# REVISED: 11MAR2019
+# REVISED: 12MAR2019
 # ==============================================================================
 
 # ==============================================================================
@@ -16,13 +16,13 @@
 # An internal constant is a variable that is intended to:
 #
 # 1. Have a fixed value;
-# 2. Be set within a makefile (e.g., "Makefile") or an "include"-ed file).
+# 2. Be set within a makefile (e.g., "Makefile") or an "include"-ed file.
 #
 # It is typically defined using the ":=" assignment operator to "simply" expand
 # its right-hand side -- i.e., immediately evaluate any variables thererin,
 # saving the resulting text as final the value.
 #
-# By convention, internal constants use uppercase words, separated by dashes.
+# By convention, its name uses uppercase, dash-separated words.
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
@@ -35,8 +35,8 @@ CARTHAGE_FILES := Cartfile Cartfile.private
 # Phony Targets
 #
 # A phony target is one that does not represent a file or directory. It can be
-# thought of as an embedded shell script to be run when an explicit request
-# is made unless uness a file of the same name exists.
+# thought of as an embedded shell script. It runs when an explicit request is
+# made unless a file of the same name exists.
 #
 # Two reasons to use a phony target are:
 #
