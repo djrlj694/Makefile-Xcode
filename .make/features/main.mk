b04f4ae62d19ca7ae90e8e9930f6721dbfa3d804
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-16 12:48:58 -0400

    Update main.mk
    Move debugging feature into `debugging.mk`.

--- .make/features/main.mk
+++ .make/features/main.mk
@@ -19,6 +19,8 @@
 # ==============================================================================
 
 include $(MAKEFILE_DIR)/features/formatting.mk
+
+include $(MAKEFILE_DIR)/features/debugging.mk
 include $(MAKEFILE_DIR)/features/helping.mk
 
 # ==============================================================================
@@ -31,13 +33,6 @@ include $(MAKEFILE_DIR)/features/helping.mk
 
 # A shortcut representing the "mkdir" command with the "-p" option.
 MKDIR := mkdir -p
-
-# ------------------------------------------------------------------------------
-# Debugging & error capture
-# ------------------------------------------------------------------------------
-
-# A list of makefile variables to show when testing/debugging.
-VARIABLES_TO_SHOW += PREFIX
 
 # ------------------------------------------------------------------------------
 # Files
@@ -56,27 +51,9 @@ LOG := make.log
 
 SHELL := bash
 
-# ------------------------------------------------------------------------------
-# Result strings
-# ------------------------------------------------------------------------------
-
-# Color-formatted outcome statuses, each of which is based on the return code
-# ($$?) of having run a shell command.
-DONE := $(FG_GREEN)done$(RESET).\n
-FAILED := $(FG_RED)failed$(RESET).\n
-IGNORE := $(FG_YELLOW)ignore$(RESET).\n
-PASSED := $(FG_GREEN)passed$(RESET).\n
-
 # ==============================================================================
 # Internal Variables
 # ==============================================================================
-
-# ------------------------------------------------------------------------------
-# Debugging & error capture
-# ------------------------------------------------------------------------------
-
-status_result = $(call result,$(DONE))
-test_result = $(call result,$(PASSED))
 
 # ------------------------------------------------------------------------------
 # Directories
@@ -104,64 +81,6 @@ subdir_var = $(FG_CYAN)$(subdir)$(RESET)
 
 # Color-formatted name of the current makefile target being run.
 target_var = $(FG_CYAN)$@$(RESET)
-
-# ==============================================================================
-# User-Defined Functions
-# ==============================================================================
-
-# $(call result,formatted-string)
-# Prints success string, $(DONE) or $(PASSED), if the most recent return code
-# value equals 0; otherwise, print $(FAILED) and the associated error message.
-define result
-	([ $$? -eq 0 ] && printf "$1") || \
-	(printf "$(FAILED)\n" && cat $(LOG) && echo)
-endef
-
-# ==============================================================================
-# Phony Targets
-# ==============================================================================
-
-# ------------------------------------------------------------------------------
-# Main phony targets
-# ------------------------------------------------------------------------------
-
-.PHONY: log
-
-## log: Shows the most recently generated log for a specified release.
-log:
-	@echo
-	#@set -e; \
-	#LOG==$$(ls -l $(LOGS_DIR)/* | head -1); \
-	#printf "Showing the most recent log: $(LOG_FILE)\n"; \
-	#echo; \
-	#cat $$LOG
-	printf "Showing the most recent log: $(LOG_FILE)\n"
-	@echo
-	@cat $(LOG_FILE)
-
-# ------------------------------------------------------------------------------
-# Prerequisite phony targets for the "debug" target
-# ------------------------------------------------------------------------------
-
-.PHONY: debug-dirs-ll debug-dirs-tree debug-vars-all debug-vars-some
-
-## debug-dirs-ll: Shows the contents of directories in a "long listing" format.
-debug-dirs-ll:
-	ls -alR $(PREFIX)
-
-## debug-dirs-tree: Shows the contents of directories in a tree-like format.
-debug-dirs-tree:
-	tree -a $(PREFIX)
-
-## debug-vars-all: Shows all Makefile variables (i.e., built-in and custom).
-debug-vars-all:
-	@echo
-	$(foreach v, $(.VARIABLES), $(info $(v) = $($(v))))
-
-## debug-vars-some: Shows only a few custom Makefile variables.
-debug-vars-some:
-	@echo
-	$(foreach v, $(VARIABLES_TO_SHOW), $(info $(v) = $($(v))))
 
 # ==============================================================================
 # Directory Targets
@@ -193,16 +112,6 @@ debug-vars-some:
 #	@mkdir -p $(@D); $(status_result)
 #	@printf "Making marker file $(target_var)..."
 #	@touch $@; $(status_result)
-
-# ==============================================================================
-# Intermediate Targets
-# ==============================================================================
-
-.INTERMEDIATE: $(LOG)
-
-# Makes a temporary file capturring a shell command error.
-$(LOG):
-	@touch $@
 
 # ==============================================================================
 # Second Expansion Targets
