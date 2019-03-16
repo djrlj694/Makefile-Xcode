d22dbb96c84d1f68f21659d166847baebc499b11
djrlj694 <robert.l.jones@synthelytics.com>
2019-03-16 13:48:57 -0400

    Create setting_up.mk

---
+++ .make/features/setting_up.mk
@@ -0,0 +1,67 @@
+# setting_up.mk
+# Copyright © 2019 Synthelytics LLC. All rights reserved.
+#
+# ==============================================================================
+# PROGRAM: setting_up.mk
+# AUTHORS: Robert L. Jones
+# COMPANY: Synthelytics LLC
+# VERSION: 1.0.0
+# CREATED: 16MAR2019
+# REVISED: 16MAR2019
+#
+# NOTES:
+#   For more info on terminology, style conventions, or source references, see
+#   the file ".make/README.md".
+# ==============================================================================
+
+# ==============================================================================
+# Internal Constants
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Commands
+# ------------------------------------------------------------------------------
+
+# A shortcut representing the "mkdir" command with the "-p" option.
+MKDIR := mkdir -p
+
+# ==============================================================================
+# Internal Variables
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Directories
+# ------------------------------------------------------------------------------
+
+# Name of the subdirectory, represented by the current Makefile target being
+# run. The shell command "basename" removes the parent directory, $(@D)), from
+# the absolute path of the makefile target.
+subdir = $(shell basename $(@D))
+
+# ------------------------------------------------------------------------------
+# Files
+# ------------------------------------------------------------------------------
+
+file = $(basename $@)
+
+# ------------------------------------------------------------------------------
+# Path strings
+# ------------------------------------------------------------------------------
+
+dir_var = $(FG_CYAN)$(@D)$(RESET)
+###file_var = $(FG_CYAN)$(file)$(RESET) # RLJ: Commented out. 23FEEB2019, RRLJ
+file_var = $(FG_CYAN)$(@F)$(RESET)
+subdir_var = $(FG_CYAN)$(subdir)$(RESET)
+
+# Color-formatted name of the current makefile target being run.
+target_var = $(FG_CYAN)$@$(RESET)
+
+# ==============================================================================
+# Directory Targets
+# ==============================================================================
+
+# Makes a directory tree.
+%/.: | $(LOG)
+	@printf "Making directory tree $(dir_var)..."
+	@$(MKDIR) $(@D) >$(LOG) 2>&1; \
+	$(status_result)
