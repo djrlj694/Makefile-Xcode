280a08adadb323d4b050a723967cc2b423244639
djrlj694 <robert.l.jones@synthelytics.com>
2019-05-25 08:53:05 -0400

    Update helping.mk
    Fixes online help.

--- .make/features/helping.mk
+++ .make/features/helping.mk
@@ -5,7 +5,7 @@
 # COMPANY: djrlj694.dev
 # VERSION: 1.0.0
 # CREATED: 16MAR2019
-# REVISED: 24MAY2019
+# REVISED: 26MAY2019
 #==============================================================================#
 # For more info on terminology, style conventions, or source references, see
 # the file ".make/README.md".
@@ -53,7 +53,7 @@ export targets_help
 define usage_help
 
 Usage:
-  make = make $(FG_CYAN)<target>$(RESET) $(MAKE_ARGS)
+  make $(MAKE_ARGS)
 
 endef
 export usage_help
@@ -68,15 +68,24 @@ export usage_help
 
 .PHONY: help
 
-## help: Shows "make" command's online help.
+## help: Shows the "make" command's online help.
 help:
 	@printf "$$usage_help"
 	@printf "$$targets_help"
+# Use the makefile set as a data source for displaying a lexicographically
+# sorted, color-formatted list of targets.
+#
+# Note:
+# 1. "cat" handles $MAKEFILE_LIST, a space-delimited list of makefiles that
+#    includes Makefile and makefiles with the extension ".mk".
+# 2. "egrep" filters for makefile lines with:
+#    a. "## " starting at column 1;
+#    b. The name of of the target preceding a colon, followed by a space.
 #	@cat $(MAKEFILE_LIST) | \
 #	egrep '^[a-zA-Z_-]+:.*?##' | \
 #	sed -e 's/:.* ##/: ##/' | sort -d | \
 #	awk 'BEGIN {FS = ":.*?## "}; {printf "  $(target_help)\n", $$1, $$2}'
 	@cat $(MAKEFILE_LIST) | \
-	egrep '^## [a-zA-Z_-]+: ' | sed -e 's/## //' | sort -d | \
+	egrep '^## [a-zA-Z_-]+: ' | sed -e 's/## //' | sort -t ':' -k1,1 -d | \
 	awk 'BEGIN {FS = ": "}; {printf "  $(target_help)\n", $$1, $$2}'
 	@echo ""
